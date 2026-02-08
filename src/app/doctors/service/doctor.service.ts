2c2889df6a9cbf5d799a6591cf37111cd8980173
ShaimaaAKamal <shaimaakamal039@gmail.com>
2026-02-09 01:45:49 +0200

    add patient and doctor services

--- src/app/doctors/service/doctor.service.ts
+++ src/app/doctors/service/doctor.service.ts
@@ -1,9 +1,103 @@
 import { Injectable } from '@angular/core';
+import { ApiDocor } from '../models/api-docor';
+import { map, Observable } from 'rxjs';
+import { ApiResponse, ApiSearchResponse } from 'src/app/common/Models/api-response';
+import ApiService from 'src/app/shared/services/api.service';
+import { Doctor } from '../models/doctor';
+import { RequestWrapper } from 'src/app/common/Models/request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
 
-  constructor() { }
+constructor(private apiService: ApiService) { }
+
+
+  getDoctors(): Observable<ApiDocor[]> {
+    return this.apiService.get<ApiResponse<ApiDocor[]>>('Doctor').pipe(
+      map((response: ApiResponse<ApiDocor[]>) => {
+        if (!response.success) {
+          throw new Error(response.message || 'API failed to load patients');
+        }
+        return response.data;
+      })
+    );
+  }
+
+  createDoctor(body: Doctor): Observable<ApiDocor> {
+    return this.apiService
+      .post<ApiResponse<ApiDocor>>('Doctor', body)
+      .pipe(
+        map((response: ApiResponse<ApiDocor>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to create doctoe';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+
+  getDoctor(id: string): Observable<ApiDocor> {
+    return this.apiService
+      .getSingle<ApiResponse<ApiDocor>>('Doctor', id)
+      .pipe(
+        map((response: ApiResponse<ApiDocor>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to load doctor';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+  updateDoctor(id: string, body: Doctor): Observable<ApiDocor> {
+    return this.apiService
+      .put<ApiResponse<ApiDocor>>('Doctor', id, body)
+      .pipe(
+        map((response: ApiResponse<ApiDocor>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to update doctor';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+  deleteDoctor(id: string): Observable<string> {
+    return this.apiService
+      .delete<ApiResponse<string>>('Doctor', id)
+      .pipe(
+        map((response: ApiResponse<string>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to delete doctor';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+  search(body: RequestWrapper): Observable<{ doctors: ApiDocor[]; total: number }> {
+    return this.apiService
+      .query<ApiSearchResponse<ApiDocor>>('Doctor/query', body)
+      .pipe(
+        map((response: ApiSearchResponse<ApiDocor>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to query';
+            throw new Error(msg);
+          }
+          return {
+            doctors: response.data.data ?? [],
+            total: response.data.totalPages ?? 0,
+          };
+        })
+      );
+  }
+
+
 }
--- src/app/patients/service/patient.service.ts
+++ src/app/patients/service/patient.service.ts
@@ -1,9 +1,112 @@
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import ApiService from 'src/app/shared/services/api.service';
+import { ApiPatient } from '../models/api-patient';
+import { ApiResponse, ApiSearchResponse } from 'src/app/common/Models/api-response';
+import { Patient } from '../models/patient';
+import { RequestWrapper } from 'src/app/common/Models/request';
+
+type PatientLookupType = 'id' | 'mrn';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
+constructor(private apiService: ApiService) { }
 
-  constructor() { }
+
+  getPatients(): Observable<ApiPatient[]> {
+    return this.apiService.get<ApiResponse<ApiPatient[]>>('Patient').pipe(
+      map((response: ApiResponse<ApiPatient[]>) => {
+        if (!response.success) {
+          throw new Error(response.message || 'API failed to load patients');
+        }
+        return response.data;
+      })
+    );
+  }
+
+  createPatient(body: Patient): Observable<ApiPatient> {
+    return this.apiService
+      .post<ApiResponse<ApiPatient>>('Patient', body)
+      .pipe(
+        map((response: ApiResponse<ApiPatient>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to create patient';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+
+
+getPatient(value: string, type: PatientLookupType = 'id'): Observable < ApiPatient > {
+  const endpoint =
+    type === 'mrn'
+      ? 'Patient/by-mrn'
+      : 'Patient';
+
+  return this.apiService
+    .getSingle<ApiResponse<ApiPatient>>(endpoint, value)
+    .pipe(map(this.mapApiResponse));
+}
+  updatePatient(id: string, body: Patient): Observable<ApiPatient> {
+    return this.apiService
+      .put<ApiResponse<ApiPatient>>('Patient', id, body)
+      .pipe(
+        map((response: ApiResponse<ApiPatient>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to update patient';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+  deletePatient(id: string): Observable<string> {
+    return this.apiService
+      .delete<ApiResponse<string>>('Patient', id)
+      .pipe(
+        map((response: ApiResponse<string>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to delete patient';
+            throw new Error(msg);
+          }
+          return response.data;
+        })
+      );
+  }
+
+  search(body: RequestWrapper): Observable<{ patients: ApiPatient[]; total: number }> {
+    return this.apiService
+      .query<ApiSearchResponse<ApiPatient>>('Patient/query', body)
+      .pipe(
+        map((response: ApiSearchResponse<ApiPatient>) => {
+          if (!response.success) {
+            const msg = response.errors?.join(', ') || response.message || 'API failed to query';
+            throw new Error(msg);
+          }
+          return {
+            patients: response.data.data ?? [],
+            total: response.data.totalPages ?? 0,
+          };
+        })
+      );
+  }
+
+   private mapApiResponse<T>(response: ApiResponse<T>): T {
+    if (!response.success) {
+      const msg =
+        response.errors?.join(', ')
+        || response.message
+        || 'API request failed';
+
+      throw new Error(msg);
+    }
+
+    return response.data;
+  }
 }
